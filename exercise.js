53247e3dce8ce7f3e01c50771c30e48e6cff0156
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-07-31 10:59:36 -0300

    Change RIR to buttons, add resetInput for Save and Clear

--- exercise.js
+++ exercise.js
@@ -2,18 +2,58 @@ const sets = [];
 
 const weightInput = document.getElementById('js-weight-input');
 const repsInput = document.getElementById('js-reps-input');
-const rirInput = document.getElementById('js-rir-input');
+
+let rir = 0;
+
+const rirDisplay = document.getElementById('rir-display');
+const increaseRirButton = document.getElementById('js-increase-rir');
+const decreaseRirButton = document.getElementById('js-decrease-rir');
+
+increaseRirButton.addEventListener('click', () => {
+  rir += 0.5;
+  rirDisplay.textContent = rir.toFixed(1);
+});
+
+decreaseRirButton.addEventListener('click', () => {
+  if (rir >= 0.5) {
+    rir -= 0.5;
+    rirDisplay.textContent = rir.toFixed(1);
+  }
+});
+
 const saveButton = document.getElementById('save-button');
+const clearButton = document.getElementById('clear-button');
 
 saveButton.addEventListener('click', () => {
-  const set = {
+  const repsValue = repsInput.value;
+  const isValidReps = Number.isInteger(Number(repsValue));
+
+  if(!isValidReps) {
+    alert('Reps must be whole numbers only.');
+    return;
+  }
+
+  let set = {
     weight: parseFloat(weightInput.value),
     reps: parseInt(repsInput.value),
-    rir: parseInt(rirInput.value)
+    rir: rir
   }
   sets.push(set);
   console.log(sets);
+
+  resetInputs();
 });
+
+clearButton.addEventListener('click', () => {
+  resetInputs();
+});
+
+function resetInputs() {
+  weightInput.value = '';
+  repsInput.value = '';
+  rir = 0;
+  rirDisplay.textContent = rir.toFixed(1);
+}
 
 repsInput.addEventListener('keydown', (e) => {
   if (e.key === '.' || e.key === ',' || e.key === '-') {
@@ -21,8 +61,8 @@ repsInput.addEventListener('keydown', (e) => {
   }
 });
 
-rirInput.addEventListener('keydown', (e) => {
-  if (e.key === ',' || e.key === '-') {
+weightInput.addEventListener('keydown', (e) => {
+  if (e.key === '-' || e.key === ',') {
     e.preventDefault();
   }
 });

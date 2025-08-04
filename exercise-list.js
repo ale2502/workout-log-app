8296db77e4975ea22d4c7cbd5b6fa6194d61b079
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-08-04 11:41:16 -0300

    Create js file with exercises

---
+++ exercise-list.js
@@ -0,0 +1,28 @@
+// exercise types: weighted, bodyweight, time_hold, distance;
+
+export const exercises = [
+  {
+    id: 1,
+    name: 'Dumbbell Bench Press',
+    muscleGroup: 'Chest',
+    type: 'weighted'
+  },
+  {
+    id: 2,
+    name: 'Pull Down',
+    muscleGroup: 'Back',
+    type: 'weighted'
+  },
+  {
+    id: 3,
+    name: 'Pull Down',
+    muscleGroup: 'Back',
+    type: 'weighted'
+  },
+  {
+    id: 4,
+    name: 'Barbell Romanian Deadlift',
+    muscleGroup: 'Hamstrings',
+    type: 'weighted'
+  }
+];

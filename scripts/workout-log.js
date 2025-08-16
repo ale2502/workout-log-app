fb4958a68b892c96683ac53288d5d2edaa54bd78
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-08-15 22:31:43 -0300

    Styling current-workout

--- scripts/workout-log.js
+++ scripts/workout-log.js
@@ -6,11 +6,11 @@ function renderWorkout() {
   workoutLog.forEach((exercise) => {
     let workoutLogHTML = '';
     workoutLogHTML += `
-      <div>${exercise.exerciseName}</div>
+      <div class="sets-reps">${exercise.exerciseName}</div>
     `;
     exercise.sets.forEach((set) => {
       workoutLogHTML += `
-        <div>Set ${set.set}: ${set.weight}kg x ${set.reps} reps (RIR: ${set.rir})</div>
+        <div class="sets-reps">Set ${set.set}: ${set.weight}kg x ${set.reps} reps (RIR: ${set.rir})</div>
       `;
     });
     workoutLogHTMLWrap += `<div class="each-exercise-and-sets">${workoutLogHTML}</div>`;

32bbc52dcd2a0585ac400e267bc24b5675f91c4f
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-09-09 12:48:35 -0300

    Using Node to connect to MongoDB

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node API');
+});
+
+mongoose.connect('')

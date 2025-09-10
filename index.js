28145f23e524af54b96477a8b2d3979b08e040d9
Alessandro Jablonski <34753426+ale2502@users.noreply.github.com>
2025-09-10 09:56:07 -0300

    Installed .env and changed DB pw

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+dotenv.config();
 
 app.get('/', (req, res) => {
   res.send('Hello from Node API');
 });
 
-mongoose.connect('')
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@workoutlogdb.smmyliq.mongodb.net/database?retryWrites=true&w=majority&appName=WorkoutLogDB`)
+.then(() => {
+  console.log('Connected to database!');
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+})
+.catch(() => {
+  console.log('Connection failed');
+});
